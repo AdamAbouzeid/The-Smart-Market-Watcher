a3ad24c032231478f97571741ec9e8e26d514f51
Adam Abouzeid <adamwabouzeid@gmail.com>
2025-12-15 11:42:04 +0200

    feat: enhance stock data processing and export to Excel

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import StockClient
+import pandas as pd
 
 def read_stock_names(file_name="watchlist.txt"):
     stock_names = []
@@ -22,8 +23,17 @@ def get_all_stock_prices(stock_names_list):
 def main():
     stock_names = read_stock_names()
     stock_prices, stock_previous_close = get_all_stock_prices(stock_names)
-    print(stock_prices, stock_previous_close)
+    stock_data = {
+        "names": stock_names,
+        "prices": stock_prices,
+        "previous_close": stock_previous_close
+    }
 
+    stock_data = pd.DataFrame(stock_data)
+    stock_data["change %"] = (stock_data.prices - stock_data.previous_close) / stock_data.previous_close * 100
+    print(stock_data)
+
+    stock_data.to_excel("stock_watch.xlsx", index=False)
 
 if __name__ == "__main__":
     main()
