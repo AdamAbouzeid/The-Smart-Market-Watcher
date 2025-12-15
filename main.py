a40c4a64ea13ca4b927c10e01f85bc16d023addf
Ahmed Wael Wanas <ahmedwaelwanas@gmail.com>
2025-12-15 12:55:38 +0200

    feat: added coloring cells based on value

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import StockClient
+import datetime
 import pandas as pd
+from openpyxl import load_workbook
+from openpyxl.styles import PatternFill
+
 
 def read_stock_names(file_name="watchlist.txt"):
     stock_names = []
@@ -15,25 +19,42 @@ def get_all_stock_prices(stock_names_list):
     stock_previous_close = []
     for stock in stock_names_list:  
         stock_data = stock_client.fetch_price(stock)
-        stock_prices.append(stock_data[0])
-        stock_previous_close.append(stock_data[1])
+        if stock_data is not None:
+            stock_prices.append(stock_data[0])
+            stock_previous_close.append(stock_data[1])
 
     return stock_prices, stock_previous_close
-
 def main():
     stock_names = read_stock_names()
     stock_prices, stock_previous_close = get_all_stock_prices(stock_names)
-    stock_data = {
+
+    stock_data = pd.DataFrame({
         "names": stock_names,
         "prices": stock_prices,
         "previous_close": stock_previous_close
-    }
+    })
 
-    stock_data = pd.DataFrame(stock_data)
-    stock_data["change %"] = (stock_data.prices - stock_data.previous_close) / stock_data.previous_close * 100
-    print(stock_data)
+    stock_data["change %"] = (
+        (stock_data.prices - stock_data.previous_close)
+        / stock_data.previous_close * 100
+    )
 
-    stock_data.to_excel("stock_watch.xlsx", index=False)
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    excel_file_name = f"stock_watch-{timestamp}.xlsx"
+    stock_data.to_excel(excel_file_name, index=False)
+
+
+    wb = load_workbook(excel_file_name)
+    sh = wb.active
+
+    positive_fill = PatternFill(start_color="00FF00", end_color="00FF00", fill_type="solid")
+    negative_fill = PatternFill(start_color="FF0000", end_color="FF0000", fill_type="solid")
+
+    for i in range(2, sh.max_column + 1):
+        if int(sh.cell(row=i, column=4).value) > 0:
+            sh.cell(row=i, column=4).fill = positive_fill
+        elif int(sh.cell(row=i, column=4).value) < 0:
+            sh.cell(row=i, column=4).fill = negative_fill
 
 if __name__ == "__main__":
     main()
